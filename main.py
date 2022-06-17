6a095183d29924d221777cb98e86fbcff2d2adf2
Anand Rajan <63197676+QuantumMonkey@users.noreply.github.com>
2022-06-17 10:49:01 +0530

    Completed Data Gathering Completed Data Cleaning Completed Exploratory Data Analysis Started Predictive Analysis Cleaned Final Merged Data Started Data Split Started Column Categorization

--- main.py
+++ main.py
@@ -10,44 +10,57 @@ import pandas as pd  # to perform data and file processing
 import matplotlib.pyplot as plt  # for plotting data
 import seaborn as sns  # for visualizing data
 
+
+def data_info(dataset):  # Display stats and null values
+    """
+    :param dataset: object type data
+    :use Prints statistical information about the specified dataset
+    :return: Nothing
+    """
+    print("Top 5 rows:\n", dataset.head(), '\n')  # Display top 5 rows
+    print("Dimensions: ", dataset.shape, '\n')  # Data Structure
+    print("Statistical details:\n", dataset.describe(), '\n')  # Display statistical details of the dataset
+    print("Schema:\n", dataset.info(), '\n')  # Display data information and datatypes
+    print("Duplicate records: ", dataset.duplicated().sum(), '\n')  # Check for duplicate records
+    print("Null values in columns:\n", dataset.isnull().sum(), '\n')  # Display number of null values in columns
+
+
+def data_clean(dataset):  # Drop null values
+    """
+        :param dataset: object type data
+        :use Removes records with null values from essential columns of the specified dataset
+        :return: Cleaned dataset
+    """
+    dataset.dropna(axis=0, how='any', inplace=True)
+    print("Null values in columns:\n", dataset.isnull().sum(), '\n')  # Display number of null values in columns
+    print("Dimensions: ", dataset.shape, '\n')  # Data Structure
+    return dataset
+
+
 # Import data into DataFrames
 titles_data = pd.read_csv('Dataset/titles.csv')
 credits_data = pd.read_csv('Dataset/credits.csv')
 
 # Data Cleaning
 # Titles
-print("Top 5 rows:\n", titles_data.head(), '\n')  # Display top 5 rows
-print("Dimensions: ", titles_data.shape, '\n')  # Data Structure
-print("Statistical details:\n", titles_data.describe(), '\n')  # Display statistical details of the dataset
-print("Schema:\n", titles_data.info(), '\n')  # Display data information and datatypes
-print("Duplicate records: ", titles_data.duplicated().sum(), '\n')  # Check for duplicate records
-print("Null values in columns:\n", titles_data.isnull().sum(), '\n')  # Display number of null values in columns
+data_info(titles_data)
 
 # Drop non-essential columns
 titles_data.drop(['description', 'age_certification', 'seasons', 'imdb_id', 'imdb_votes'], axis=1, inplace=True)
 print("Top 5 rows:\n", titles_data.head(), '\n')  # Display top 5 rows
 
-titles_data.dropna(axis=0, how='any', inplace=True)
-print("Null values in columns:\n", titles_data.isnull().sum(), '\n')  # Display number of null values in columns
-print("Dimensions: ", titles_data.shape, '\n')  # Data Structure
+titles_data = data_clean(titles_data)
 
 modelling_titles_data = titles_data  # Backup if the merged data doesn't work out
 
 # Credits
-print("Top 5 rows:\n", credits_data.head(), '\n')  # Display top 5 rows
-print("Dimensions: ", credits_data.shape, '\n')  # Data Structure
-print("Statistical details:\n", credits_data.describe(), '\n')  # Display statistical details of the dataset
-print("Schema:\n", credits_data.info(), '\n')  # Display data information and datatypes
-print("Duplicate records: ", credits_data.duplicated().sum(), '\n')  # Check for duplicate records
-print("Null values in columns:\n", credits_data.isnull().sum(), '\n')  # Display number of null values in columns
+data_info(credits_data)
 
 # Drop non-essential columns
 credits_data.drop(['character', 'person_id'], axis=1, inplace=True)
 print("Top 5 rows:\n", credits_data.head(), '\n')  # Display top 5 rows
 
-credits_data.dropna(axis=0, how='any', inplace=True)
-print("Null values in columns:\n", credits_data.isnull().sum(), '\n')  # Display number of null values in columns
-print("Dimensions: ", credits_data.shape, '\n')  # Data Structure
+data_clean(credits_data)
 
 """
 Exploratory Data Analysis - time to plot some insights
@@ -60,7 +73,7 @@ top10_production_countries.plot(kind='bar', width=0.8, figsize=(9, 6), color='r'
 plt.title('Top 10 countries with highest production')  # Title
 plt.xlabel('Countries')  # X-axis label
 plt.ylabel('Production Count')  # Y-axis label
-plt.show()
+#plt.show()
 
 # Content production per year
 year_count = titles_data.release_year.value_counts()
@@ -71,7 +84,7 @@ plt.title('Total shows/movies released over the years')  # Title
 plt.xlim(1950, 2030)  # Limit range for X-axis
 plt.xlabel('release year')  # X-axis label
 plt.ylabel('total')  # Y-axis label
-plt.show()
+#plt.show()
 
 # Types of content on Netflix
 type_count = titles_data.type.value_counts()
@@ -79,7 +92,7 @@ print("\nTypes of content with count:\n", type_count.head())
 
 type_count.plot(kind='pie', figsize=(10, 5), autopct='%1.1f%%')  # Plot Pie Chart
 plt.title('Type Distribution')  # Title
-plt.show()
+#plt.show()
 
 # Highly voted content
 
@@ -92,7 +105,7 @@ top10_tmdb_rating.plot(kind='barh', x='title', y='tmdb_popularity', figsize=(9, 
 plt.title('Top 10 based on tmdb votes')
 plt.xlabel('tmdb_popularity')  # X-axis label
 plt.ylabel('Title')  # Y-axis label
-plt.show()
+#plt.show()
 
 # Merging data for top actors and directors
 titles_data = titles_data.merge(credits_data, how='outer', on='id')
@@ -111,7 +124,7 @@ top10_directors.plot(kind='barh', x='name', y='tmdb_popularity', figsize=(9, 6),
 plt.title("Top 10 Directors based on tmdb popularity")  # Title
 plt.xlabel('tmdb_popularity')  # X-axis label
 plt.ylabel('name')  # Y-axis label
-plt.show()
+#plt.show()
 
 # Top 10 Actors
 top10_actors = actor.sort_values(['tmdb_score', 'tmdb_popularity'], ascending=False)[
@@ -123,7 +136,7 @@ top10_actors.plot(kind='barh', x='name', y='tmdb_popularity', figsize=(9, 6),
 plt.title("Top 10 Actors based on tmdb popularity")  # Title
 plt.xlabel('tmdb_popularity')  # X-axis label
 plt.ylabel('name')  # Y-axis label
-plt.show()
+#plt.show()
 
 """
 Now that we know what our data does, let us try to predict tmdb scores based on our final and merged data.
@@ -132,26 +145,56 @@ If that doesn't work, let us try to predict it on the cleaned titles_data. We wi
 
 # Merged Dataset Cleaning
 
-print(titles_data.info())
-print("Null values in columns:\n", titles_data.isnull().sum(), '\n')  # Display number of null values in columns
-titles_data.dropna(axis=0, how='any', inplace=True)
-print("Null values in columns:\n", titles_data.isnull().sum(), '\n')  # Display number of null values in columns
-print("Dimensions: ", titles_data.shape, '\n')  # Data Structure
+print(modelling_titles_data.info())
+print("Null values in columns:\n", modelling_titles_data.isnull().sum(),
+      '\n')  # Display number of null values in columns
+print("Dimensions: ", modelling_titles_data.shape, '\n')  # Data Structure
+
+print(modelling_titles_data.columns)
 
 # Data Split
 
+y = modelling_titles_data.tmdb_score
 
+modelling_titles_data['type_cat'] = modelling_titles_data.type.cat.codes
+modelling_titles_data['genres_cat'] = modelling_titles_data.genres.cat.codes
+modelling_titles_data['production_countries_cat'] = modelling_titles_data.production_countries.cat.codes
+
+print(modelling_titles_data.columns)
+
+features = ['type_cat', 'release_year', 'runtime', 'genres_cat',
+            'production_countries_cat', 'imdb_score', 'tmdb_popularity']
+X = modelling_titles_data[features]
 
 # Train-Test Split
 
 from sklearn.model_selection import train_test_split
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=True, test_size=0.2)
 
-# Trial 1 - Linear Regression
+# Trial 1 - Decision Tree Regression
+from sklearn.tree import DecisionTreeRegressor
+tmdb_model = DecisionTreeRegressor(random_state=1)
+tmdb_model.fit(X, y)
+predictions = tmdb_model.predict(X)
+print(predictions)
+"""
 
-from sklearn.linear_model import LinearRegression as LR  # Model Specification
-from sklearn.model_selection import cross_val_score as CVS  # Choose correct Root Mean Square Deviation by automating an average calculation
-
-lr = LR()
-lr.fit()
-scores = CVS()
+    # Mean Squared Error
+    from sklearn.metrics import mean_squared_error
+    mse = mean_squared_error(y_test, y_predict)
+    rmse = np.sqrt(mse)
+    print("Root Mean Squared Error(RMSE) values: ", np.round(rmse, 2))
+    
+    
+    #calc average first then compare to mea to determine randomstate
+    
+    # Cross Validation Score
+    from sklearn.model_selection import \
+        cross_val_score  # Choose correct Root Mean Square Deviation by automating an average calculation
+    scores = cross_val_score(tmdb_model, X, y, scoring='neg_mean_squared_error', cv=5, n_jobs=1)
+    
+    rmse = np.sqrt(-scores)
+    print("Root Mean Squared Error(RMSE) values: ", np.round(rmse, 2))
+    print("Average RMSE: ", np.mean(rmse))
+"""
