72843a79928af395bd5e5c9e2d74da0db3c3dddc
Anand Rajan <63197676+QuantumMonkey@users.noreply.github.com>
2022-06-18 07:54:53 +0530

    Completed Data Gathering Completed Data Cleaning Completed Exploratory Data Analysis Started Predictive Analysis Cleaned Final Merged Data Started Data Split Completed Column Categorization Started column Formatting

--- main.py
+++ main.py
@@ -13,9 +13,8 @@ import seaborn as sns  # for visualizing data
 
 def data_info(dataset):  # Display stats and null values
     """
-    :param dataset: object type data
-    :use Prints statistical information about the specified dataset
-    :return: Nothing
+    :param dataset: The table to print statistical information about.
+    :return: Displayed statistics and structure
     """
     print("Top 5 rows:\n", dataset.head(), '\n')  # Display top 5 rows
     print("Dimensions: ", dataset.shape, '\n')  # Data Structure
@@ -27,14 +26,34 @@ def data_info(dataset):  # Display stats and null values
 
 def data_clean(dataset):  # Drop null values
     """
-        :param dataset: object type data
-        :use Removes records with null values from essential columns of the specified dataset
-        :return: Cleaned dataset
+        :param dataset: The table with null values to be removed/cleaned.
+        :return: Cleaned dataset with no null values.
     """
     dataset.dropna(axis=0, how='any', inplace=True)
     print("Null values in columns:\n", dataset.isnull().sum(), '\n')  # Display number of null values in columns
     print("Dimensions: ", dataset.shape, '\n')  # Data Structure
     return dataset
+
+
+def data_format(dataset, column):
+    """
+
+    :param dataset: The table to perform SQL operations on. Used to search unique records to factorize.
+    :param column: The column we need to factorize
+    :return: Nothing. The column will be factorized and typecast-ed in the dataframe directly.
+    """
+    from pandasql import sqldf
+
+    pysqldf = lambda q: sqldf(q, globals())
+
+    unique_values_query = "select distinct {} from {}".format(column, dataset)  # Find unique values
+    unique_values_df = pysqldf(unique_values_query)  # Run PandaSQL command
+    print(unique_values_df)  # Result type is dataframe
+
+    unique_values_list = unique_values_df[column].tolist()  # Convert to list
+
+    for seek in len(unique_values_list):  # Traverse list length
+        unique_values_list[seek] = seek  # Replace list item with index (numerical)
 
 
 # Import data into DataFrames
@@ -73,7 +92,7 @@ top10_production_countries.plot(kind='bar', width=0.8, figsize=(9, 6), color='r'
 plt.title('Top 10 countries with highest production')  # Title
 plt.xlabel('Countries')  # X-axis label
 plt.ylabel('Production Count')  # Y-axis label
-#plt.show()
+# plt.show()
 
 # Content production per year
 year_count = titles_data.release_year.value_counts()
@@ -84,7 +103,7 @@ plt.title('Total shows/movies released over the years')  # Title
 plt.xlim(1950, 2030)  # Limit range for X-axis
 plt.xlabel('release year')  # X-axis label
 plt.ylabel('total')  # Y-axis label
-#plt.show()
+# plt.show()
 
 # Types of content on Netflix
 type_count = titles_data.type.value_counts()
@@ -92,7 +111,7 @@ print("\nTypes of content with count:\n", type_count.head())
 
 type_count.plot(kind='pie', figsize=(10, 5), autopct='%1.1f%%')  # Plot Pie Chart
 plt.title('Type Distribution')  # Title
-#plt.show()
+# plt.show()
 
 # Highly voted content
 
@@ -105,7 +124,7 @@ top10_tmdb_rating.plot(kind='barh', x='title', y='tmdb_popularity', figsize=(9, 
 plt.title('Top 10 based on tmdb votes')
 plt.xlabel('tmdb_popularity')  # X-axis label
 plt.ylabel('Title')  # Y-axis label
-#plt.show()
+# plt.show()
 
 # Merging data for top actors and directors
 titles_data = titles_data.merge(credits_data, how='outer', on='id')
@@ -124,7 +143,7 @@ top10_directors.plot(kind='barh', x='name', y='tmdb_popularity', figsize=(9, 6),
 plt.title("Top 10 Directors based on tmdb popularity")  # Title
 plt.xlabel('tmdb_popularity')  # X-axis label
 plt.ylabel('name')  # Y-axis label
-#plt.show()
+# plt.show()
 
 # Top 10 Actors
 top10_actors = actor.sort_values(['tmdb_score', 'tmdb_popularity'], ascending=False)[
@@ -136,7 +155,7 @@ top10_actors.plot(kind='barh', x='name', y='tmdb_popularity', figsize=(9, 6),
 plt.title("Top 10 Actors based on tmdb popularity")  # Title
 plt.xlabel('tmdb_popularity')  # X-axis label
 plt.ylabel('name')  # Y-axis label
-#plt.show()
+# plt.show()
 
 """
 Now that we know what our data does, let us try to predict tmdb scores based on our final and merged data.
@@ -159,12 +178,20 @@ features = ['type', 'release_year', 'runtime', 'genres',
             'production_countries', 'imdb_score', 'tmdb_popularity']
 X = modelling_titles_data[features]
 
-X['type'] = X['type'].astype('category')
-X['genres'] = X['genres'].astype('category')
-X['production_countries'] = X['production_countries'].astype('category')
+# Define a function for data category formatting. Select and find distinct values. Convert result into list. Traverse
+# that list and change all unique entries into float/int
 
-print(X.info())
+# X['type'] = X['type'].astype('category')
+# X['type'] = X['type'].astype('float64')
+# X['genres'] = X['genres'].astype('category')
+# X['genres'] = X['genres'].astype('float64')
+# X['production_countries'] = X['production_countries'].astype('category')
+# X['production_countries'] = X['production_countries'].astype('float64')
 
+# print(X.info())
+# print(X['type'])
+
+"""
 # Train-Test Split
 
 from sklearn.model_selection import train_test_split
@@ -177,23 +204,23 @@ tmdb_model = DecisionTreeRegressor(random_state=1)
 tmdb_model.fit(X, y)
 predictions = tmdb_model.predict(X)
 print(predictions)
-"""
 
-    # Mean Squared Error
-    from sklearn.metrics import mean_squared_error
-    mse = mean_squared_error(y_test, y_predict)
-    rmse = np.sqrt(mse)
-    print("Root Mean Squared Error(RMSE) values: ", np.round(rmse, 2))
-    
-    
-    #calc average first then compare to mea to determine randomstate
-    
-    # Cross Validation Score
-    from sklearn.model_selection import \
-        cross_val_score  # Choose correct Root Mean Square Deviation by automating an average calculation
-    scores = cross_val_score(tmdb_model, X, y, scoring='neg_mean_squared_error', cv=5, n_jobs=1)
-    
-    rmse = np.sqrt(-scores)
-    print("Root Mean Squared Error(RMSE) values: ", np.round(rmse, 2))
-    print("Average RMSE: ", np.mean(rmse))
+
+# Mean Squared Error
+from sklearn.metrics import mean_squared_error
+mse = mean_squared_error(y_test, y_predict)
+rmse = np.sqrt(mse)
+print("Root Mean Squared Error(RMSE) values: ", np.round(rmse, 2))
+
+
+#calc average first then compare to mea to determine randomstate
+
+# Cross Validation Score
+from sklearn.model_selection import \
+    cross_val_score  # Choose correct Root Mean Square Deviation by automating an average calculation
+scores = cross_val_score(tmdb_model, X, y, scoring='neg_mean_squared_error', cv=5, n_jobs=1)
+
+rmse = np.sqrt(-scores)
+print("Root Mean Squared Error(RMSE) values: ", np.round(rmse, 2))
+print("Average RMSE: ", np.mean(rmse))
 """
